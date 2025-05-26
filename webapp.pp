67800c775b5029b1cf399c1e9f3f336db8eff205
cokee96 <alvaroalonsogonzalez96@gmail.com>
2025-05-26 20:03:37 +0200

    tsting puppet

---
+++ webapp.pp
@@ -0,0 +1,26 @@
+class webapp {
+  $automation_repository = 'https://github.com/cokee96/automation-web-page.git'
+
+  package { ['httpd', 'php', 'php-mysql', 'git']:
+    ensure => installed,
+  }
+
+  service { 'httpd':
+    ensure => running,
+    enable => true,
+  }
+
+  selboolean { 'httpd_can_network_connect_db':
+    value => 'on',
+  }
+
+  file { '/var/www/html/':
+    ensure => 'directory',
+  }
+
+  exec { 'clone_repository':
+    command => "/usr/bin/git clone ${automation_repository} /var/www/html/",
+    creates => '/var/www/html/index.php',
+    require => Package['git'],
+  }
+}
