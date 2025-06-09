90731a8741039029fab870be8112244b0263d649
cokee96 <alvaroalonsogonzalez96@gmail.com>
2025-06-09 20:46:21 +0200

    testing

--- copy.sh
+++ copy.sh
@@ -5,7 +5,9 @@ PUPPET_ENV_DIR="/etc/puppetlabs/code/environments"
 
 # deploylamp / database
 mkdir -p "$PUPPET_ENV_DIR/deploylamp/modules/database/manifests"
+rm "$PUPPET_ENV_DIR/deploylamp/modules/database/templates/*"
 mkdir -p "$PUPPET_ENV_DIR/deploylamp/modules/database/templates"
+rm "$PUPPET_ENV_DIR/deploylamp/modules/database/templates/*"
 cp "$LOCAL_BASE/lamp/database.pp" "$PUPPET_ENV_DIR/deploylamp/modules/database/manifests/"
 cp "$LOCAL_BASE/lamp/webapp.pp" "$PUPPET_ENV_DIR/deploylamp/modules/database/manifests/"
 cp "$LOCAL_BASE/lamp/site.pp" "$PUPPET_ENV_DIR/deploylamp/modules/database/manifests/"
@@ -13,13 +15,16 @@ cp "$LOCAL_BASE/lamp/nodes_email.sql.erb" "$PUPPET_ENV_DIR/deploylamp/modules/da
 
 # nginx / example_web
 mkdir -p "$PUPPET_ENV_DIR/nginx/modules/example_web/manifests"
+rm "$PUPPET_ENV_DIR/nginx/modules/example_web/manifests/*"
 mkdir -p "$PUPPET_ENV_DIR/nginx/modules/example_web/templates"
+rm "$PUPPET_ENV_DIR/nginx/modules/example_web/templates/*"
 cp "$LOCAL_BASE/nginx/nginx_deploy.pp" "$PUPPET_ENV_DIR/nginx/modules/example_web/manifests/"
 cp "$LOCAL_BASE/nginx/site.pp" "$PUPPET_ENV_DIR/nginx/modules/example_web/manifests/"
 cp "$LOCAL_BASE/nginx/index.html.erb" "$PUPPET_ENV_DIR/nginx/modules/example_web/templates/"
 
 # requisites
 mkdir -p "$PUPPET_ENV_DIR/requisites/modules/requisites/manifests"
+rm "$PUPPET_ENV_DIR/requisites/modules/requisites/manifests/*"
 cp "$LOCAL_BASE/requisites/requisites.pp" "$PUPPET_ENV_DIR/requisites/modules/requisites/manifests/"
 cp "$LOCAL_BASE/requisites/site.pp" "$PUPPET_ENV_DIR/requisites/modules/requisites/manifests/"
 
