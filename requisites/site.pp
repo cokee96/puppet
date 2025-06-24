855f314d986a2ef67a30e304d3d1d9adbc78fff8
cokee96 <alvaroalonsogonzalez96@gmail.com>
2025-06-24 19:53:05 +0200

    .

--- requisites/site.pp
+++ requisites/site.pp
@@ -1,7 +1,7 @@
 # import 'requisites.pp'
 
 node 'puppet-node', 'puppet-node-2','puppet-node-3','puppet-node-4'  {
-  include requisites:requisites
+  include requisites::requisites
 }
 node default {
   notify { 'Default node configuration applied': }
